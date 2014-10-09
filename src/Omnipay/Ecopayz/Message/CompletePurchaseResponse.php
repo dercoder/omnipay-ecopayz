f0d5b0ff13092b1810999df325545747c91acf06
DerCoder <alexander.fedra@gmail.com>
2014-10-09 20:27:54 +0200

    ecoPayz Merchant Integration Version 2.0.3

---
+++ src/Omnipay/Ecopayz/Message/CompletePurchaseResponse.php
@@ -0,0 +1,35 @@
+<?php
+namespace Omnipay\Ecopayz\Message;
+
+/**
+ * Ecopayz Complete Purchase Response
+ *
+ * @author    Alexander Fedra <contact@dercoder.at>
+ * @copyright 2014 DerCoder
+ * @license   http://opensource.org/licenses/mit-license.php MIT
+ * @version   2.0.3 Ecopayz API Specification
+ */
+class CompletePurchaseResponse extends FetchTransactionResponse
+{
+
+    /**
+     * Get the Merchant’s transaction ID.
+     *
+     * @return string transaction reference
+     */
+    public function getTransactionId()
+    {
+        return (string)$this->data->ClientTransactionID;
+    }
+
+    /**
+     * Get the unique ID that identifies the transaction in the Ecopayz system.
+     *
+     * @return string transaction reference
+     */
+    public function getTransactionReference()
+    {
+        return (string)$this->data->SvsTxID;
+    }
+
+}
