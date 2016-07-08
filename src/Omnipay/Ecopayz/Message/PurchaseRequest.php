51938ec680c334a9fbcb7aa2ac50cb5e8e550ff7
Kiril Ustalov <k.ustalovs@bdit.biz>
2016-07-08 13:14:45 +0300

    Added dynamic mode functionality

--- src/Omnipay/Ecopayz/Message/PurchaseRequest.php
+++ src/Omnipay/Ecopayz/Message/PurchaseRequest.php
@@ -65,6 +65,11 @@ class PurchaseRequest extends AbstractRequest
         $data['Amount'] = $this->getAmount();
         $data['Currency'] = $this->getCurrency();
         $data['MerchantFreeText'] = $this->getDescription();
+        $data['OnSuccessUrl'] = $this->getOnSuccessUrl();
+        $data['OnFailureUrl'] = $this->getOnFailureUrl();
+        $data['TransferUrl'] = $this->getTransferUrl();
+        $data['CancellationUrl'] = $this->getCancellationUrl();
+        $data['CallbackUrl'] = $this->getCallbackUrl();
         $data['Checksum'] = $this->calculateArrayChecksum($data);
 
         return $data;
@@ -79,5 +84,114 @@ class PurchaseRequest extends AbstractRequest
     public function sendData($data)
     {
         return new PurchaseResponse($this, $data);
+    }
+
+        /**
+     * SET/GET Callback URL's for dynamic mode
+     */
+
+    /**
+     * Set the success cb url
+     *
+     * @param  string $value success cb url
+     * @return self
+     */
+    public function setOnSuccessUrl($value)
+    {
+        return $this->setParameter('OnSuccessUrl', $value);
+    }
+
+    /**
+     * Get the success cb url
+     *
+     * @return string success cb url
+     */
+    public function getOnSuccessUrl()
+    {
+        return $this->getParameter('OnSuccessUrl');
+    }
+
+    /**
+     * Set the failure cb url
+     *
+     * @param  string $value failure cb url
+     * @return self
+     */
+    public function setOnFailureUrl($value)
+    {
+        return $this->setParameter('OnFailureUrl', $value);
+    }
+
+    /**
+     * Get the failure cb url
+     *
+     * @return string failure cb url
+     */
+    public function getOnFailureUrl()
+    {
+        return $this->getParameter('OnFailureUrl');
+    }
+
+    /**
+     * Set the transfer cb url
+     *
+     * @param  string $value transfer cb url
+     * @return self
+     */
+    public function setTransferUrl($value)
+    {
+        return $this->setParameter('TransferUrl', $value);
+    }
+
+    /**
+     * Get the transfer cb url
+     *
+     * @return string transfer cb url
+     */
+    public function getTransferUrl()
+    {
+        return $this->getParameter('TransferUrl');
+    }
+
+    /**
+     * Set the cancellation cb url
+     *
+     * @param  string $value cancellation cb url
+     * @return self
+     */
+    public function setCancellationUrl($value)
+    {
+        return $this->setParameter('CancellationUrl', $value);
+    }
+
+    /**
+     * Get the cancellation cb url
+     *
+     * @return string cancellation cb url
+     */
+    public function getCancellationUrl()
+    {
+        return $this->getParameter('CancellationUrl');
+    }
+
+    /**
+     * Set the callback cb url
+     *
+     * @param  string $value callback cb url
+     * @return self
+     */
+    public function setCallbackUrl($value)
+    {
+        return $this->setParameter('CallbackUrl', $value);
+    }
+
+    /**
+     * Get the callback cb url
+     *
+     * @return string callback cb url
+     */
+    public function getCallbackUrl()
+    {
+        return $this->getParameter('CallbackUrl');
     }
 }
